9c95608ab74c14e14244996bf48cae117a8f655f
PK006 <plai6588@gmail.com>
2024-03-28 11:29:49 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Wedsite Developing using Python")
-st.header("Wedsite Developing using Python")
-st.subheader("Phurin Kwanjira")
+st.title("🌜🌜Wedsite Developing using Python"🌛🌛)
+st.header("🌟Wedsite Developing using Python🌟")
+st.subheader("🌝Phurin Kwanjira🌝")
 st.image('012.jpg')
