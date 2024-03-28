14c475fef2ac46b72ccab3b0ac643821a69eb1b2
PK006 <plai6588@gmail.com>
2024-03-28 11:39:35 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🌜🌜Wedsite Developing using Python")
 st.header("🌟Wedsite Developing using Python🌟")
+
 st.subheader("🌝Phurin Kwanjira🌝")
 st.image('012.jpg')
+
+dt=pd.read_csv('./data/iris.csv')
+st.write(dt.head(10))
